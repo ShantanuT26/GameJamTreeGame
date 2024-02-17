e7674c9ef75c07a1d696c6c2ac6448e7f9915947
Shantanu <95384818+ShantanuT26@users.noreply.github.com>
2024-02-16 23:23:54 -0700

    Update playerBehavior.cs
    I did more shit I guess

--- Assets/playerBehavior.cs
+++ Assets/playerBehavior.cs
@@ -6,15 +6,18 @@ public class playerBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool isGrounded = false;
-    public Rigidbody rb;
+    public Rigidbody2D rb;
     public Vector3 jump;
+    public Vector3 move;
+    public float moveDir;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0f, 2f, 0f);
+        rb = GetComponent<Rigidbody2D>();
+        jump = new Vector3(0f, 10f, 0f);
+        move = new Vector3(0f,0f,0f);
     }
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         isGrounded=true;
         Debug.Log("True!");
@@ -22,13 +25,18 @@ public class playerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            moveDir=0.05f;
+            move.x=moveDir;
+            gameObject.transform.Translate(move);
+        }
+    }   
     void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.Space)&&isGrounded)
         {
-            rb.AddForce(jump, ForceMode.Impulse);
+            rb.AddForce(jump, ForceMode2D.Impulse);
             isGrounded=false;
         }
     }
