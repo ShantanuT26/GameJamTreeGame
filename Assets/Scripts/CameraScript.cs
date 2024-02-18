43c6ee07450b2722951fa8e817500573d2a86dec
Shantanu <95384818+ShantanuT26@users.noreply.github.com>
2024-02-18 11:47:47 -0700

    I did more stuff

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z-10);
     }
 }
